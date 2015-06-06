ebe4f6988e95807c800b8f1f14c5718baf351545
john <johnyucn@163.com>
2015-06-06 18:58:04 +0800

    ssds

---
+++ struts_spring/src/action/LoginAction.java
@@ -0,0 +1,45 @@
+package action;
+
+import service.UserManager;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport {
+	
+	private String uname;
+	private String psd;
+	private UserManager um;
+	
+	
+	
+	public void setUm(UserManager um) {
+		this.um = um;
+	}
+
+
+	public String getUname() {
+		return uname;
+	}
+
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+
+	public String getPsd() {
+		return psd;
+	}
+
+
+	public void setPsd(String psd) {
+		this.psd = psd;
+	}
+
+
+	public String login(){
+		um.login(uname, psd);
+		
+		return SUCCESS;
+	}
+}
---
+++ struts_spring/src/service/impl/UserManagerImpl.java
@@ -0,0 +1,13 @@
+package service.impl;
+
+import service.UserManager;
+
+public class UserManagerImpl implements UserManager {
+
+	@Override
+	public void login(String uname, String psd) {
+		System.out.println("manager ...login.......");
+
+	}
+
+}

04b1e7bf4b4c6ad635f2c5629c0ca3854d3bab70
john <johnyucn@163.com>
2015-06-07 10:57:50 +0800

    对CURD进行了单元测试

--- hibernatedemo1/src/main/java/test/HibernateTest1.java
+++ hibernatedemo1/src/main/java/test/HibernateTest1.java
@@ -4,21 +4,71 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import po.User;
 
 public class HibernateTest1 {
-
-	public static void main(String[] args) {
+	private static SessionFactory sf;
+	private Session sen;
+	@BeforeClass
+	public static void  beforeClass(){
+		sf=new Configuration().configure().buildSessionFactory();
+	}
+	
+	@AfterClass
+	public static void  afterClass(){
+		sf.close();
+	}
+	
+	@Before
+	public void before(){
+		sen=sf.openSession();
+	}
+	@After
+	public void after(){
+		sen.close();
+	}
+	
+	@Test // junit,测试框架
+	public void testSave(){
 		User u = new User("john",100,true);
-		
-		
-		SessionFactory sf=new Configuration().configure().buildSessionFactory();
-		Session sen=sf.openSession();
 		Transaction tx=sen.beginTransaction();
 		sen.save(u);
 		tx.commit();
-		sen.close();
+		
+		Assert.assertTrue(u.getId()!=null);
+	}
+	@Test // junit,测试框架
+	public void testLoad(){
+		User u=(User)sen.get(User.class, 1);
+		System.out.println(u.getUname());
+		Assert.assertTrue(u.getUname()!=null);
+	}
+	
+	@Test // junit,测试框架
+	public void testUpdate(){
+		Transaction tx=sen.beginTransaction();
+		User u=(User)sen.get(User.class, 1);
+		u.setUname("tom");
+		tx.commit();
+	}
+	
+	@Test // junit,测试框架
+	public void testDelete(){
+		Transaction tx=sen.beginTransaction();
+		User u=(User)sen.get(User.class, 2);
+		sen.delete(u);
+		tx.commit();
+	}
+	
+	public static void main(String[] args) {
+		
 
 	}
 
